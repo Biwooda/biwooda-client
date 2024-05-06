9f52cf1a77922a3ec90acb1b8a4fd952ddd746cc
haesa <xdhaesa@gmail.com>
2024-05-07 01:48:54 +0900

    [ADD] DefaultButton 컴포넌트에 onClick prop 추가

--- src/components/DefaultButton/DefaultButton.jsx
+++ src/components/DefaultButton/DefaultButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './DefaultButton.module.css';
 
-export default function DefaultButton({ text, disabled }) {
+export default function DefaultButton({ text, onClick, disabled }) {
   return (
-    <button className={styles.button} disabled={disabled}>
+    <button className={styles.button} onClick={onClick} disabled={disabled}>
       <div className={styles.text}>{text}</div>
     </button>
   );
