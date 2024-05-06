7804e1843e4c2627d1406600f7c1795d8a89e07d
haesa <xdhaesa@gmail.com>
2024-05-06 18:33:40 +0900

    [ADD]: 회원가입 피드백 메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,8 +1,17 @@
-const FEEDBACK_MESSAGE = {
+const LOGIN_FEEDBACK_MESSAGE = {
   emailPass: '올바른 이메일입니다',
   emailFail: '올바른 형태의 이메일이 아닙니다',
   passwordPass: '올바른 비밀번호',
   passwordFail: '영소문자/숫자/특수문자 조합 8자리 이상',
 };
 
-export { FEEDBACK_MESSAGE };
+const SIGN_UP_FEEDBACK_MESSAGE = {
+  emailPass: '이메일 확인 완료',
+  emailFail: '올바른 형태의 이메일이 아닙니다',
+  code: (time) => `${time} 안에 인증번호를 입력해주세요`,
+  codePass: '확인 완료',
+  passwordPass: '가능한 비밀번호입니다',
+  passwordFail: '8자리 이상의 비밀번호를 만들어주세요',
+};
+
+export { LOGIN_FEEDBACK_MESSAGE, SIGN_UP_FEEDBACK_MESSAGE };
