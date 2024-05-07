262725ec2ad11b8e60343bec8ada206fc04e1afc
haesa <xdhaesa@gmail.com>
2024-05-07 14:21:12 +0900

    [ADD] 회원가입 및 비밀번호 재설정 페이지의 타이틀 문자열 상수 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,11 @@
 import buttonStyles from './loginButton';
 import { LOGIN_FEEDBACK_MESSAGE, SIGN_UP_FEEDBACK_MESSAGE } from './message';
+import { RESET_PASSWORD_PAGE_TITLE, SIGN_UP_PAGE_TITLE } from './title';
 
-export { LOGIN_FEEDBACK_MESSAGE, SIGN_UP_FEEDBACK_MESSAGE, buttonStyles };
+export {
+  LOGIN_FEEDBACK_MESSAGE,
+  RESET_PASSWORD_PAGE_TITLE,
+  SIGN_UP_FEEDBACK_MESSAGE,
+  SIGN_UP_PAGE_TITLE,
+  buttonStyles,
+};
