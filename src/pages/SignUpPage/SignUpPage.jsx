45f302189a46730ce62ff4e813a98e5c6a01c1da
haesa <xdhaesa@gmail.com>
2024-05-16 00:10:00 +0900

    #26 [ADD] SignUpPage에 회원가입 완료 CTA 버튼 추가

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import CTAButton from '../../components/CTAButton/CTAButton';
 import EmailVerification from '../../components/EmailVerification/EmailVerification';
 import PasswordVerification from '../../components/PasswordVerification/PasswordVerification';
 import { SIGN_UP_PAGE_TITLE } from '../../constants';
 import SubPage from '../../pages/SubPage/SubPage';
+import { checkPasswordFormat } from '../../utils/checkFormat';
 import styles from './SignUpPage.module.css';
 
 export default function SignUpPage() {
@@ -25,6 +27,14 @@ export default function SignUpPage() {
           <PasswordVerification data={formData} onChange={handleChange} />
         </div>
       </div>
+      <CTAButton
+        text='이메일 회원가입 완료하기'
+        onClick={() => console.log('success sign up!')}
+        disabled={
+          !checkPasswordFormat(formData.password) ||
+          formData.password !== formData.rePassword
+        }
+      />
     </SubPage>
   );
 }
