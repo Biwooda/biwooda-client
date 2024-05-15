27144dc8864e9711a1c45d128ca9d07c82302d32
haesa <xdhaesa@gmail.com>
2024-05-15 23:59:19 +0900

    #26 [FEAT] 인증번호 필드에 3분 타이머 적용

--- src/components/EmailVerification/EmailVerification.jsx
+++ src/components/EmailVerification/EmailVerification.jsx
@@ -1,27 +1,51 @@
 import React, { useState } from 'react';
 import DefaultButton from '../../components/DefaultButton/DefaultButton';
 import FeedbackMessage from '../../components/FeedbackMessage/FeedbackMessage';
-import TextField from '../../components/TextField/TextField';
 import { SIGN_UP_FEEDBACK_MESSAGE } from '../../constants';
+import useTimer from '../../hooks/useTimer';
 import { checkEmailFormat } from '../../utils/checkFormat';
+import toTime from '../../utils/convertNumberToTime';
+import InputField from '../InputField/InputField';
 import styles from './EmailVerification.module.css';
 
 export default function EmailVerification({ data, onChange }) {
+  const [time, startTimer, clearTimer] = useTimer(180);
+  const [isDisabled, setDisabled] = useState(false);
   const [isSended, setIsSended] = useState(false);
-  const handleChange = (event) => {
-    setIsSended(false);
+  const [verification, setVerification] = useState(false);
+  const onChangeEmail = (event) => {
     onChange(event);
+    setDisabled(false);
+  };
+
+  const onSendCode = () => {
+    setDisabled(true);
+    setIsSended(true);
+    startTimer();
+  };
+
+  const onResendCode = () => {
+    setVerification(false);
+    startTimer();
+  };
+
+  const checkCode = (event) => {
+    onChange(event);
+    if (event.target.value === '1111') {
+      setVerification(true);
+      clearTimer();
+    }
   };
 
   return (
     <div className={styles.codeFieldset}>
       <div>
-        <TextField
+        <InputField.Text
           name='email'
           label='이메일'
           value={data.email}
           placeholder='이메일을 입력해주세요'
-          onChange={handleChange}
+          onChange={onChangeEmail}
         >
           <FeedbackMessage
             message={
@@ -31,31 +55,39 @@ export default function EmailVerification({ data, onChange }) {
             }
             condition={checkEmailFormat(data.email)}
           />
-        </TextField>
+        </InputField.Text>
         <div className={styles.codeButton}>
           <div className={styles.sendCodeButton}>
             <DefaultButton
               text='이메일로 인증번호 전송'
-              onClick={() => setIsSended(true)}
-              disabled={!checkEmailFormat(data.email) || isSended}
+              onClick={onSendCode}
+              disabled={!checkEmailFormat(data.email) || isDisabled}
             />
           </div>
-          {isSended && (
+          {isDisabled && (
             <div className={styles.resendCodeButton}>
-              <DefaultButton text='재발송' />
+              <DefaultButton text='재발송' onClick={onResendCode} />
             </div>
           )}
         </div>
       </div>
-      <TextField
+      <InputField.Code
         name='code'
         label='인증번호'
         value={data.code}
         placeholder='인증번호를 입력해주세요'
-        onChange={onChange}
+        isSended={isSended}
+        onChange={checkCode}
       >
-        <FeedbackMessage message='확인 완료' condition={true} />
-      </TextField>
+        <FeedbackMessage
+          message={
+            verification
+              ? '확인 완료'
+              : `${toTime(time)}안에 인증번호를 입력해주세요`
+          }
+          condition={verification}
+        />
+      </InputField.Code>
     </div>
   );
 }
