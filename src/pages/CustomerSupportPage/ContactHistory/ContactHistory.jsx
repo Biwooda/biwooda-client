b6e1a3c6f7c5f46b315a6bdd308a0363bca44c02
haesa <xdhaesa@gmail.com>
2024-05-29 21:55:42 +0900

    #46 [ADD] 문의내역 페이지 - map에 key 추가

--- src/pages/CustomerSupportPage/ContactHistory/ContactHistory.jsx
+++ src/pages/CustomerSupportPage/ContactHistory/ContactHistory.jsx
@@ -5,12 +5,14 @@ import styles from './ContactHistory.module.css';
 
 const datas = [
   {
+    key: 1,
     order: '1',
     title: '분실신고 문의 드립니다.',
     date: '23.06.23 13:08:25',
     state: '처리중',
   },
   {
+    key: 2,
     order: '2',
     title: '분실신고 문의드립니다.',
     date: '23.01.07 16:15:31',
@@ -28,8 +30,8 @@ export default function ContactHistory() {
         <div className={styles.state}>처리상태</div>
       </BlueLabel>
       <div>
-        {datas.map(({ order, title, date, state }) => (
-          <ListItem>
+        {datas.map(({ key, order, title, date, state }) => (
+          <ListItem key={key}>
             <div className={styles.order}>{order}</div>
             <div className={styles.title}>{title}</div>
             <div className={styles.date}>{date}</div>
