aa9fd24663e6213e399a70c5f4f3f4440efc48b4
haesa <xdhaesa@gmail.com>
2024-05-29 21:48:42 +0900

    #46 [FEAT] 고객센터 페이지 y축 스크롤

--- src/pages/CustomerSupportPage/CustomerSupportPage.jsx
+++ src/pages/CustomerSupportPage/CustomerSupportPage.jsx
@@ -9,34 +9,36 @@ export default function CustomerSupportPage() {
   const underlineRef = useRef();
 
   return (
-    <section>
-      <div className={styles.toolbar}>
-        <BackToolbar title='고객센터' onPrev={() => navigate('/')} />
-      </div>
-      <div className={styles.tabBar}>
-        <div className={styles.tabs}>
-          <Link
-            to='/customer-support'
-            className={`${styles.tab} ${tab === 'Tab1' && styles.selected}`}
-            onClick={(event) => {
-              underlineRef.current.style.transform = 'translateX(calc(33%))';
-              setTab('Tab1');
-            }}
-          >
-            문의하기
-          </Link>
-          <Link
-            to='/customer-support/contact-history'
-            className={`${styles.tab} ${tab === 'Tab2' && styles.selected}`}
-            onClick={() => {
-              underlineRef.current.style.transform = 'translateX(200%)';
-              setTab('Tab2');
-            }}
-          >
-            문의내역
-          </Link>
+    <section className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.toolbar}>
+          <BackToolbar title='고객센터' onPrev={() => navigate('/')} />
         </div>
-        <div className={styles.underline} ref={underlineRef}></div>
+        <div className={styles.tabBar}>
+          <div className={styles.tabs}>
+            <Link
+              to='/customer-support'
+              className={`${styles.tab} ${tab === 'Tab1' && styles.selected}`}
+              onClick={(event) => {
+                underlineRef.current.style.transform = 'translateX(calc(33%))';
+                setTab('Tab1');
+              }}
+            >
+              문의하기
+            </Link>
+            <Link
+              to='/customer-support/contact-history'
+              className={`${styles.tab} ${tab === 'Tab2' && styles.selected}`}
+              onClick={() => {
+                underlineRef.current.style.transform = 'translateX(200%)';
+                setTab('Tab2');
+              }}
+            >
+              문의내역
+            </Link>
+          </div>
+          <div className={styles.underline} ref={underlineRef}></div>
+        </div>
       </div>
       <Outlet />
     </section>
