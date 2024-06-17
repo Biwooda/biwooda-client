a3818e59e4bc0b59ad4ab30f86108db8daf633c7
haesa <xdhaesa@gmail.com>
2024-06-18 00:56:21 +0900

    #45 [STYLE] CTAButton에 white 스타일 추가

--- src/components/CTAButton/CTAButton.jsx
+++ src/components/CTAButton/CTAButton.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './CTAButton.module.css';
 
-export default function CTAButton({ children, onClick, disabled }) {
+export default function CTAButton({ children, type, onClick, disabled }) {
   return (
-    <button className={styles.button} onClick={onClick} disabled={disabled}>
+    <button
+      className={`${styles.button} ${type === 'white' && styles.white}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <div className={styles.text}>{children}</div>
     </button>
   );
