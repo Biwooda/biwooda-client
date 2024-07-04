399f620ee95fc1941d726b639264116e7978fd05
haesa <xdhaesa@gmail.com>
2024-07-05 00:24:56 +0900

    #80 [STYLE] App.jsx의 classname을 id로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <UserContextProvider>
       <DrawerContextProvider>
-        <div className='App'>
+        <div id='App'>
           <Outlet />
         </div>
       </DrawerContextProvider>
