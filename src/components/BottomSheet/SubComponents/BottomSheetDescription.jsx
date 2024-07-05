d0cddbb316bf7d5a0b77981c0c41bc700c008d3d
haesa <xdhaesa@gmail.com>
2024-07-05 18:26:34 +0900

    #84 [FEAT] BottomSheetDescription hasCheckbox 옵션 추가

--- src/components/BottomSheet/SubComponents/BottomSheetDescription.jsx
+++ src/components/BottomSheet/SubComponents/BottomSheetDescription.jsx
@@ -8,14 +8,30 @@ export default function BottomSheetDescription({
   children,
   hasArrowButton,
   to,
+  hasCheckbox,
+  isCheck,
+  checkText,
+  handleCheckbox,
 }) {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${hasCheckbox && styles.hasCheck}`}>
       <pre className={styles.description}>{children}</pre>
       {hasArrowButton && (
         <Link to={to} className={styles.arrowButton}>
           <Icon id='rightArrow' stroke='#92A5B3' width={18} height={18} />
         </Link>
+      )}
+      {hasCheckbox && (
+        <label htmlFor='checkbox'>
+          <input
+            id='checkbox'
+            type='checkbox'
+            className={styles.checkbox}
+            checked={isCheck}
+            onChange={handleCheckbox}
+          />
+          <span className={styles.text}>{checkText}</span>
+        </label>
       )}
     </div>
   );
